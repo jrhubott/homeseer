99b3bdda245156ab45c50a1ffa23bce87f211239
Jason Rhubottom <3378004+jrhubott@users.noreply.github.com>
2020-12-21 16:27:15 +0000

    use base class

--- sensor.py
+++ sensor.py
@@ -20,6 +20,8 @@ from homeassistant.const import (
 
 from homeassistant.helpers.entity import Entity
 
+from .hoomseer import HomeseerEntity
+
 from .const import _LOGGER, DOMAIN, UNIT_PERCENTAGE
 
 DEPENDENCIES = ["homeseer"]
@@ -39,50 +41,12 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     async_add_entities(sensor_devices)
 
 
-class HSSensor(Entity):
+class HSSensor(HomeseerEntity, Entity):
     """Base representation of a HomeSeer sensor-type device."""
 
     def __init__(self, device, connection):
         self._device = device
         self._connection = connection
-
-    @property
-    def available(self):
-        """Return whether the device is available."""
-        return self._connection.api.state == STATE_LISTENING
-
-    @property
-    def device_state_attributes(self):
-        attr = {
-            "Device Ref": self._device.ref,
-            "Location": self._device.location,
-            "Location 2": self._device.location2,
-        }
-        return attr
-
-    @property
-    def unique_id(self):
-        """Return a unique ID for the device."""
-        return f"{self._connection.namespace}-{self._device.ref}"
-
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return self._connection.name_template.async_render(device=self._device).strip()
-
-    @property
-    def state(self):
-        """Return the state of the device."""
-        return self._device.value
-
-    @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
-
-    async def async_added_to_hass(self):
-        """Register value update callback."""
-        self._device.register_update_callback(self.async_schedule_update_ha_state)
 
 
 class HSBattery(HSSensor):
