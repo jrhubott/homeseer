61e2635e13a84c5a42932854f5321aec36d5a395
Jason Rhubottom <3378004+jrhubott@users.noreply.github.com>
2020-12-21 16:27:57 +0000

    update binary sensor

--- binary_sensor.py
+++ binary_sensor.py
@@ -4,6 +4,7 @@ Support for HomeSeer binary-type devices.
 
 from .pyhs3ng import HASS_BINARY_SENSORS, STATE_LISTENING
 
+from .hoomseer import HomeseerEntity
 from homeassistant.components.binary_sensor import BinarySensorEntity
 
 from .const import _LOGGER, DOMAIN
@@ -25,7 +26,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     async_add_entities(binary_sensor_devices)
 
 
-class HSBinarySensor(BinarySensorEntity):
+class HSBinarySensor(HomeseerEntity, BinarySensorEntity):
     """Representation of a HomeSeer binary-type device."""
 
     def __init__(self, device, connection):
@@ -33,39 +34,6 @@ class HSBinarySensor(BinarySensorEntity):
         self._connection = connection
 
     @property
-    def available(self):
-        """Return True if the HomeSeer connection is available."""
-        return self._connection.api.state == STATE_LISTENING
-
-    @property
-    def device_state_attributes(self):
-        attr = {
-            "Device Ref": self._device.ref,
-            "Location": self._device.location,
-            "Location 2": self._device.location2,
-        }
-        return attr
-
-    @property
-    def unique_id(self):
-        """Return a unique ID for the device."""
-        return f"{self._connection.namespace}-{self._device.ref}"
-
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return self._connection.name_template.async_render(device=self._device).strip()
-
-    @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
-
-    @property
     def is_on(self):
         """Return true if device is on."""
         return self._device.value > 0
-
-    async def async_added_to_hass(self):
-        """Register value update callback."""
-        self._device.register_update_callback(self.async_schedule_update_ha_state)
