4b86d02a43edaa130d81c0cb72131e7c90ce3b12
Jason Rhubottom <3378004+jrhubott@users.noreply.github.com>
2020-12-21 16:27:28 +0000

    light use base class

--- light.py
+++ light.py
@@ -3,6 +3,7 @@ Support for HomeSeer light-type devices.
 """
 
 from .pyhs3ng import HASS_LIGHTS, STATE_LISTENING
+from .hoomseer import HomeseerEntity
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
     SUPPORT_BRIGHTNESS,
@@ -28,41 +29,12 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     async_add_entities(light_devices)
 
 
-class HSLight(LightEntity):
+class HSLight(HomeseerEntity, LightEntity):
     """Representation of a HomeSeer light-type device."""
 
     def __init__(self, device, connection):
         self._device = device
         self._connection = connection
-
-    @property
-    def available(self):
-        """Return whether the device is available."""
-        return self._connection.api.state == STATE_LISTENING
-
-    @property
-    def device_state_attributes(self):
-        attr = {
-            "Device Ref": self._device.ref,
-            "Location": self._device.location,
-            "Location 2": self._device.location2,
-        }
-        return attr
-
-    @property
-    def unique_id(self):
-        """Return a unique ID for the device."""
-        return f"{self._connection.namespace}-{self._device.ref}"
-
-    @property
-    def name(self):
-        """Return the name of the device."""
-        return self._connection.name_template.async_render(device=self._device).strip()
-
-    @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
 
     @property
     def supported_features(self):
@@ -91,7 +63,3 @@ class HSLight(LightEntity):
     async def async_turn_off(self, **kwargs):
         """Turn the light off."""
         await self._device.off()
-
-    async def async_added_to_hass(self):
-        """Register value update callback."""
-        self._device.register_update_callback(self.async_schedule_update_ha_state)
